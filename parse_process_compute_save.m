2283539295d21682a370df6a26ea617c7867f3d5
bradleybarth <bradley.barth@duke.edu>
2018-05-08 16:33:40 -0400

    Add files via upload
    Downloaded files and made changes to streamline MATLAB and Python code, specifically looking at day-to-day variability.

--- parse_process_compute_save.m
+++ parse_process_compute_save.m
@@ -1,20 +1,18 @@
-function parse_process_compute_save(ID, experimentSet, notch14)
+function parse_process_compute_save(ID, notch14)
 % master executable to run all MATLAB processes
-% ID refers to specimen number (Rat_ID), and experiment set is a string to 
-% determine which experiments to process - S: standard, L: lidocaine, C: 
-% cholinergic blockers. notch14 is a boolean to apply a notch filter at 14
-% Hz, or not
+% ID refers to specimen number (Rat_ID), and notch14 is a boolean to apply 
+% a notch filter at 14 Hz, or not
 
-parse_gi_data(ID, experimentSet)
-preprocess_gi_data(SAVEFILE, notch14)
-compute_spectra
 lbl = {'S','L','C'};
-expSet = [];
 for i = 1:3
-  if max(lbl{i} == experimentSet)
-    expSet = [expSet lbl{i}];
-  end
+    parse_gi_data(ID, lbl{i})
+    disp([lbl{i},': parsed '])
+    preprocess_gi_data(notch14)
+    disp([lbl{i},': preprocessed '])
+    compute_spectra
+    disp([lbl{i},': spectra computed '])
+    name = [num2str(ID), '_Data_', lbl{i}, '_Notch14_', num2str(notch14)];
+    disp(name)
+    save2Py(name)
+    disp([lbl{i},': saved '])
 end
-name = [num2str(ID), '_Data_', expSet, '_Notch14_', num2str(notch14)];
-disp(name)
-save2Py(name)
